64c00a46b36e4980aacb3832c5d378abf9a0de0b
Galllya <galina.bolganova@gmail.com>
2021-07-13 15:42:47 +0700

    Commit message.

--- lib/news_page/news.dart
+++ lib/news_page/news.dart
@@ -12,48 +12,40 @@ class News extends StatefulWidget {
 class _HomePageState extends State<News> {
   final _scrollController = ScrollController();
   final dio = Dio();
-  PaginationNewsModel<NewsViewModel>? newsPagination;
-  List<NewsViewModel> news = [];
+
+  int currentPage = 1;
+  PaginationModel<NewsViewModel>? newsPagination;
+
   bool haveConnection = true;
   bool loadingNews = false;
   bool loadingEvent = false;
-  int currentPage = 1;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    loadnews(currentPage);
+    loadNews(currentPage);
+
     _scrollController.addListener(_onScroll);
   }
 
-  Future<void> loadnews(int page) async {
-    final String URL =
-        'http://abiturient.paraweb.media/api/v1/News?page=1&size=100';
+  void loadNews(int page) async {
+    final String url =
+        'http://abiturient.paraweb.media/api/v1/News?page=$page&size=10';
     try {
-      var response = await Dio().get(URL);
+      var response = await dio.get(url);
 
       final pagination = PaginationModel.fromJson(response.data,
           (json) => NewsViewModel.fromJson(json as Map<String, dynamic>));
-
-      await Future.delayed(const Duration(seconds: 1));
-
-      currentPage = currentPage + 1;
       setState(() {
-        news = pagination as List<NewsViewModel>;
-        newsPagination = PaginationNewsModel<NewsViewModel>(
-          elements: [
-            /// Так как при первой загрузке у нас еще нет значения в этом поле, проверяем его на null и добавляем, если есть
-            if (newsPagination != null) ...newsPagination!.elements,
-            ...news,
-          ],
-          totalCount: news.length,
-        );
+        currentPage = page;
+        newsPagination = PaginationModel<NewsViewModel>([
+          if (newsPagination != null) ...newsPagination!.elements,
+          ...pagination.elements,
+        ], pagination.totalCount);
         loadingNews = true;
       });
     } catch (e) {
-      haveConnection = false;
       loadingNews = true;
       throw Exception('Ошибка загрузки новостей');
     }
@@ -61,7 +53,7 @@ class _HomePageState extends State<News> {
 
   void _onScroll() {
     if (_isBottom) {
-      loadnews(currentPage + 1);
+      loadNews(currentPage + 1);
     }
   }
 
@@ -82,25 +74,32 @@ class _HomePageState extends State<News> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Новости"),
-          centerTitle: true,
-        ),
-        body: ListView.separated(
-            controller: _scrollController,
-            padding: const EdgeInsets.all(16),
-            itemBuilder: (BuildContext context, int index) {
-              /// выводим индикатор загрузки, если индекс выходит за длину массива
-              if (index >= news.length)
-                return const CircularProgressIndicator();
+      appBar: AppBar(
+        title: Text("Новости"),
+        centerTitle: true,
+      ),
+      body: newsPagination != null
+          ? ListView.separated(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16),
+              itemBuilder: (BuildContext context, int index) {
+                /// выводим индикатор загрузки, если индекс выходит за длину массива
+                if (index >= newsPagination!.elements.length)
+                  return const CircularProgressIndicator();
 
-              final element = NewsSectionCard(
-                newsViewModel: news.elementAt(index),
-              );
-              return element;
-            },
-            separatorBuilder: (context, index) => const SizedBox(height: 16),
-            itemCount: news.length));
+                final element = NewsSectionCard(
+                  newsViewModel: newsPagination!.elements.elementAt(index),
+                );
+                return element;
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 16),
+              itemCount:
+                  newsPagination!.totalCount > newsPagination!.elements.length
+                      ? newsPagination!.elements.length + 1
+                      : newsPagination!.elements.length,
+            )
+          : CircularProgressIndicator(),
+    );
   }
 }
 
