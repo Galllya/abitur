6ba371975c490248adb21a8f1762bfb53a26ae85
Galllya <galina.bolganova@gmail.com>
2021-08-02 12:04:35 +0700

    gg

---
+++ lib/domain/token.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'token.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TokenData _$TokenDataFromJson(Map<String, dynamic> json) {
+  return TokenData(
+    token: json['token'] as String,
+  );
+}
